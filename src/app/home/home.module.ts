7b106fcb89471b5c65eb9a301c6378e7c6cf7859
Sergio Velasquez <sergio.velasquez@pragma.com.co>
2021-02-05 12:01:28 -0500

    Primer commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ReactiveFormsModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,64 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  myDate: String = new Date().toISOString();
 
-  constructor() {}
+  contactos = JSON.parse(localStorage.getItem('contactos')!) || [];
 
+  createFormGroup() {
+    return new FormGroup({
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(35),
+      ]),
+      fechaNacimiento: new FormControl(''),
+      numeroTel: new FormControl('', Validators.required),
+      tipoTelefono: new FormControl(''),
+      esPrincipal: new FormControl(''),
+    });
+  }
+
+  contactForm: FormGroup;
+
+  constructor() {
+    this.contactForm = this.createFormGroup();
+  }
+
+  onResetForm() {
+    this.contactForm.reset();
+  }
+
+  calculateAge() {
+    let currentDate = new Date();
+    let bornDate = new Date(this.contactForm.get('fechaNacimiento').value);
+
+    console.log(currentDate.getFullYear());
+    console.log(bornDate.getFullYear());
+  }
+
+  guardarContato() {
+    if (this.contactForm.valid) {
+      this.contactos.push(this.contactForm.value);
+      localStorage.setItem('contactos', JSON.stringify(this.contactForm.value));
+      this.onResetForm();
+    } else {
+      console.log('Formulario no valido');
+    }
+  }
+
+  get nombre() {
+    return this.contactForm.get('nombre');
+  }
+  get fechaNacimiento() {
+    return this.contactForm.get('fechaNacimiento');
+  }
+  get numeroTel() {
+    return this.contactForm.get('numeroTel');
+  }
+  get tipoTelefono() {
+    return this.contactForm.get('tipoTelefono');
+  }
+  get esPrincipal() {
+    return this.contactForm.get('esPrincipal');
+  }
 }
